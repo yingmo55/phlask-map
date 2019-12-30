786cbb2d4383b753a8a84a3dda5a58330a1fdce2
unknown <taylor.alan.johnson@gmail.com>
2019-12-29 22:59:19 -0500

    add button and start work on modal

--- src/components/ClosestTap.js
+++ src/components/ClosestTap.js
@@ -1,56 +1,90 @@
 import React, { Component } from "react";
-import {Card} from "react-bootstrap";
-
+import { Card, Modal, Button } from "react-bootstrap";
 
 export class ClosestTap extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        text : "Click for nearest tap!"
+      text: <p style={{ display: "inline-block" }}>Click for nearest tap!</p>,
+      show: false
     };
 
     this.change = this.change.bind(this);
-    }
+    this.handleClose = this.handleClose.bind(this);
+    this.handleShow = this.handleShow.bind(this);
+  }
 
-  change(){
-    if(this.props.lat === "" || this.props.lon === ""){
+  change() {
+    if (this.props.lat === "" || this.props.lon === "") {
       this.setState({
-        text: <p>
-        The closest tap feature is unavailable. We require permission
-        to access your location to provide it.
-      </p>
-      })
-    }
-    else{
+        text: (
+          <p>
+            The closest tap feature is unavailable. We require permission to
+            access your location to provide it.
+          </p>
+        )
+      });
+    } else {
       this.setState({
-        text: <p>
-        The closest tap is: {this.props.org} <br />
-        Located at:   &nbsp;
-        <a
-          href={
-            "https://www.google.com/maps/search/?api=1&query=" +
-            this.props.lat +
-            ", " +
-            this.props.lon
-          }
-        >
-          {this.props.address}
-        </a>
-      </p>  
+        text: (
+          <p style={{ display: "inline-block" }}>
+            The closest tap is: {this.props.org} <br />
+            Located at: &nbsp;
+            <a
+              href={
+                "https://www.google.com/maps/search/?api=1&query=" +
+                this.props.lat +
+                ", " +
+                this.props.lon
+              }
+            >
+              {this.props.address}
+            </a>
+          </p>
+        )
       });
     }
   }
 
+  handleClose() {
+    this.setState({ show: false });
+  }
+  handleShow() {
+    this.setState({ show: true });
+  }
+
   render() {
     return (
-      <div className = "closestTap">
+      <div className="closestTap">
         <Card>
-          <Card.Header onClick={this.change}>
-              {this.state.text}
+          <Card.Header
+            onClick={this.change}
+            style={{ display: "inline", paddingBottom: "0px" }}
+          >
+            {this.state.text}
+            <img
+              onClick={this.handleShow}
+              title="Add a new tap"
+              className="add-tap"
+              src="https://image.flaticon.com/icons/png/512/61/61112.png"
+            ></img>
           </Card.Header>
-            
         </Card>
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
